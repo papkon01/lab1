946ece595ad13046c094ce65e28e5833dff46317
papad <papadopouloskon2001@gmail.com>
2025-02-24 15:01:07 +0200

    final code

--- lab1.py
+++ lab1.py
@@ -1,14 +1,5 @@
-import requests  
-
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
+import requests
+from datetime import datetime
 
 url = input("Enter URL:\t ")
   
@@ -17,19 +8,22 @@ if not url.startswith('https://'):
     
 print(url)
 
-
-with requests.get(url) as response:  # το αντικείμενο response
-    print("\nRESPONSE HEADER")
+with requests.get(url) as response:  
+    print("\n=== RESPONSE HEADER ===")
     for key, value in response.headers.items():
         print(f"{key:30s} {value}")
 
 server_info = response.headers.get("Server", "Unknown")
-print(f"\nServer Information: {server_info}")
+print(f"\n=== Server OS ===\n {server_info}")
 
 cookies = response.cookies
 if cookies:
     print("\n=== Cookies Set by the Server ===")
     for cookie in cookies:
-        print(f"{cookie.name}: {cookie.value}")
+        if hasattr(cookie, 'expires'):
+            expires_date = datetime.fromtimestamp(cookie.expires).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            expires_date = "No expiration date"
+        print(f"{cookie.name}: {cookie.value} (Expires: {expires_date})")
 else:
     print("\nNo cookies were set by the server.")
